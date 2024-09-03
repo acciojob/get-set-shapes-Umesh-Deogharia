84430eef0fe5f3f73e0fd852706916bb160e4e62
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-03 12:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	get width(){
+		return this._width;
+	}
+	
+	get height(){
+		return this._height;
+	}
+
+	getArea(){
+		return (this.width)*(this.height);
+	}
+	
+}
+
+class Square extends Rectangle {
+	constructor(side){
+		this._side = side;
+	}
+
+	get width(){
+		return this._side;
+	}
+	
+	get height(){
+		return this._side;
+	}
+
+	getArea(){
+		return (this.side)*(this.side);
+	}
+	
+	getPerimeter(){
+		return 4*this._side;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
